2c57d6287b1e6258c17ba6534c206a49b8ad3cd9
Yu.Lygin <yuriylygin.com>
2022-12-13 18:47:20 +0000

    ашч

--- src/emulator.py
+++ src/emulator.py
@@ -34,26 +34,6 @@ def generate_customer_profiles_table(n_customers, random_state=0):
     return customer_profiles_table
 
 
-# def get_list_terminals_within_radius(customer_profile, x_y_terminals, r):
-    
-#     # Use numpy arrays in the following to speed up computations
-    
-#     # Location (x,y) of customer as numpy array
-#     x_y_customer = customer_profile[['x_customer_id','y_customer_id']].values.astype(float)
-    
-#     # Squared difference in coordinates between customer and terminal locations
-#     squared_diff_x_y = np.square(x_y_customer - x_y_terminals)
-    
-#     # Sum along rows and compute suared root to get distance
-#     dist_x_y = np.sqrt(np.sum(squared_diff_x_y, axis=1))
-    
-#     # Get the indices of terminals which are at a distance less than r
-#     available_terminals = list(np.where(dist_x_y<r)[0])
-    
-#     # Return the list of terminal IDs
-#     return available_terminals
-    
-
 def generate_terminal_profiles_table(n_terminals, random_state=0):
     
     np.random.seed(random_state)
