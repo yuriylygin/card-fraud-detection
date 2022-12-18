abb2344f36076249777cacbe77c0b6f8d4904c60
Yu.Lygin <yuriylygin@gmail.com>
2022-12-18 16:26:56 +0000

    chore

--- dags/update.py
+++ dags/update.py
@@ -21,35 +21,37 @@ args = {
 with DAG(
     dag_id="update-dataset",
     default_args=args,
-    schedule_interval="*/30 * * * *",
+    schedule_interval="@once",
     start_date=datetime(2021, 1, 1),
     tags=["API"],
 ) as dag:
     create_pyspark_job = DataprocCreatePysparkJobOperator(
         task_id="create_pyspark_job",
-        main_python_file_uri="s3a://data-proc-public/jobs/sources/pyspark-001/main.py",
-        python_file_uris=[
-            "s3a://data-proc-public/jobs/sources/pyspark-001/geonames.py",
-        ],
-        file_uris=[
-            "s3a://data-proc-public/jobs/sources/data/config.json",
-        ],
-        archive_uris=[
-            "s3a://data-proc-public/jobs/sources/data/country-codes.csv.zip",
-        ],
-        args=[
-            "s3a://data-proc-public/jobs/sources/data/cities500.txt.bz2",
-            f"s3a://{S3_BUCKET_NAME_FOR_JOB_LOGS}/dataproc/job/results/${{JOB_ID}}",
-        ],
-        jar_file_uris=[
-            "s3a://data-proc-public/jobs/sources/java/dataproc-examples-1.0.jar",
-            "s3a://data-proc-public/jobs/sources/java/icu4j-61.1.jar",
-            "s3a://data-proc-public/jobs/sources/java/commons-lang-2.6.jar",
-        ],
-        properties={
-            "spark.submit.deployMode": "cluster",
-        },
-        packages=["org.slf4j:slf4j-simple:1.7.30"],
-        repositories=["https://repo1.maven.org/maven2"],
-        exclude_packages=["com.amazonaws:amazon-kinesis-client"],
+        cluster_id="c9q1ee8pnj1r46ogk49s",
+        name="airflow-update",
+        main_python_file_uri="s3a://yl-otus/update-dataset.py",
+        # python_file_uris=[
+        #     "s3a://data-proc-public/jobs/sources/pyspark-001/geonames.py",
+        # ],
+        # file_uris=[
+        #     "s3a://data-proc-public/jobs/sources/data/config.json",
+        # ],
+        # archive_uris=[
+        #     "s3a://data-proc-public/jobs/sources/data/country-codes.csv.zip",
+        # ],
+        # args=[
+        #     "s3a://data-proc-public/jobs/sources/data/cities500.txt.bz2",
+        #     f"s3a://{S3_BUCKET_NAME_FOR_JOB_LOGS}/dataproc/job/results/${{JOB_ID}}",
+        # ],
+        # jar_file_uris=[
+        #     "s3a://data-proc-public/jobs/sources/java/dataproc-examples-1.0.jar",
+        #     "s3a://data-proc-public/jobs/sources/java/icu4j-61.1.jar",
+        #     "s3a://data-proc-public/jobs/sources/java/commons-lang-2.6.jar",
+        # ],
+        # properties={
+        #     "spark.submit.deployMode": "cluster",
+        # },
+        # packages=["org.slf4j:slf4j-simple:1.7.30"],
+        # repositories=["https://repo1.maven.org/maven2"],
+        # exclude_packages=["com.amazonaws:amazon-kinesis-client"],
     )
---
+++ src/data/yandex-example.py
@@ -0,0 +1,32 @@
+import sys
+from pyspark import SparkConf, SparkContext
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print('Usage job.py <input_dir> <output_dir>')
+        sys.exit(1)
+
+    in_dir = sys.argv[1]
+    out_dir = sys.argv[2]
+
+    conf = SparkConf().setAppName("Word count - PySpark")
+    sc = SparkContext(conf=conf)
+    appID = sc._jsc.sc().applicationId()
+    out_dir += "/" + appID
+
+    text_file = sc.textFile(in_dir)
+    counts = text_file.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+
+    if out_dir.startswith('s3a://'):
+        counts.saveAsTextFile(out_dir) 
+    else:
+        default_fs = sc._jsc.hadoopConfiguration().get('fs.defaultFS')
+        counts.saveAsTextFile(default_fs + out_dir)
+
+
+if __name__ == "__main__":
+    main()
