b481da7565e52bc45fe029502a7e6e955a557f44
Yu Lygin <yuriylygin@gmail.com>
2023-01-04 07:31:18 +0000

    fix

--- dags/update.py
+++ dags/update.py
@@ -27,29 +27,26 @@ with DAG(
 ) as dag:
     create_pyspark_job = DataprocCreatePysparkJobOperator(
         task_id="create_pyspark_job",
-        cluster_id="c9q1ee8pnj1r46ogk49s",
+        cluster_id="c9qe7r6747r6hidd2p05",
         name="airflow-update",
-        main_python_file_uri="s3a://yl-otus/update-dataset.py",
-        # python_file_uris=[
-        #     "s3a://data-proc-public/jobs/sources/pyspark-001/geonames.py",
-        # ],
+        main_python_file_uri="s3a://yl-otus/update.py",
+        python_file_uris=[
+            "s3a://yl-otus/cfd.zip",
+        ],
         # file_uris=[
         #     "s3a://data-proc-public/jobs/sources/data/config.json",
         # ],
-        archive_uris=[
-            "s3a://yl-otus/venv.zip",
-        ],
-        # args=[
-        #     "s3a://data-proc-public/jobs/sources/data/cities500.txt.bz2",
-        #     f"s3a://{S3_BUCKET_NAME_FOR_JOB_LOGS}/dataproc/job/results/${{JOB_ID}}",
+        # archive_uris=[
+        #     "s3a://yl-otus/venv.zip",
         # ],
+        args=["30", "2022-01-01", "10"],
         # jar_file_uris=[
         #     "s3a://data-proc-public/jobs/sources/java/dataproc-examples-1.0.jar",
         #     "s3a://data-proc-public/jobs/sources/java/icu4j-61.1.jar",
         #     "s3a://data-proc-public/jobs/sources/java/commons-lang-2.6.jar",
         # ],
         properties={
-            "spark.submit.deployMode": "cluster",
+            "spark.submit.deployMode": "client",
         },
         # packages=["org.slf4j:slf4j-simple:1.7.30"],
         # repositories=["https://repo1.maven.org/maven2"],
